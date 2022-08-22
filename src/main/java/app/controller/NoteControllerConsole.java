514a6ad656d8927f01927041e74d6b718fec7355
sirbmircea <sirbmircea@yahoo.com>
2022-08-22 22:10:24 +0300

    logs relocation for REST app

--- src/main/java/app/controller/NoteControllerConsole.java
+++ src/main/java/app/controller/NoteControllerConsole.java
@@ -6,13 +6,9 @@ import app.model.Note;
 import app.service.NoteService;
 import app.service.ServiceProvider;
 
-import java.util.Set;
-import java.util.logging.Logger;
-
 public class NoteControllerConsole {
-    private static final Logger logger = Logger.getLogger(NoteControllerConsole.class.getName());
     private NoteService noteService = ServiceProvider.getInstance().get(NoteService.class);
-    private final String ADD_RECEIVES_2_PARAMETERS = "The method add receives 2 parameters!";
+    private static final String ADD_RECEIVES_2_PARAMETERS = "The method add receives 2 parameters!";
 
 
     public NoteControllerConsole(String... args) throws NoArgumentsException, InvalidNumberOfArgumentsException, NoSuchMethodException {
@@ -36,13 +32,7 @@ public class NoteControllerConsole {
 
     public void litsAll(String... args) throws InvalidNumberOfArgumentsException, NoSuchMethodException {
         if (args[0].equals("list")) {
-            Set<Note> noteSet = noteService.list();
-            if (!noteSet.isEmpty()) {
-                noteSet.forEach(note -> logger.info(note.toString()));
-            } else {
-                logger.info("List is empty!");
-            }
-
+            noteService.list();
         } else if (args[0].equals("add")) {
             throw new InvalidNumberOfArgumentsException(ADD_RECEIVES_2_PARAMETERS);
         } else {
@@ -52,13 +42,7 @@ public class NoteControllerConsole {
 
     public void listSingleNote(String... args) throws InvalidNumberOfArgumentsException, NoSuchMethodException {
         if (args[0].equals("list")) {
-            Note note = noteService.list(args[1]);
-            if (!note.getNoteTitle().equals("")) {
-                logger.info(note.toString());
-            } else {
-                logger.info(String.format("Couldn't find the note with the title: %s!", args[1]));
-            }
-
+            noteService.list(args[1]);
         } else if (args[0].equals("add")) {
             throw new InvalidNumberOfArgumentsException(ADD_RECEIVES_2_PARAMETERS);
         } else {
@@ -68,13 +52,7 @@ public class NoteControllerConsole {
 
     public void add(String... args) throws InvalidNumberOfArgumentsException, NoSuchMethodException {
         if (args[0].equals("add")) {
-            Note note = noteService.add(new Note(args[1], args[2]));
-            if (note.getNoteTitle().equals("")) {
-                logger.warning("We have a duplicate!");
-            } else {
-                logger.info("The note was added!");
-            }
-
+            noteService.add(new Note(args[1], args[2]));
         } else if (args[0].equals("list")) {
             throw new InvalidNumberOfArgumentsException("The method list receives 0 or 1 parameters!");
         } else {
--- src/main/java/app/service/impl/NoteServiceImpl.java
+++ src/main/java/app/service/impl/NoteServiceImpl.java
@@ -5,8 +5,10 @@ import app.repository.NoteRepository;
 import app.service.NoteService;
 
 import java.util.Set;
+import java.util.logging.Logger;
 
 public class NoteServiceImpl implements NoteService {
+    private static final Logger logger = Logger.getLogger(NoteServiceImpl.class.getName());
 
     NoteRepository noteRepository;
 
@@ -17,21 +19,35 @@ public class NoteServiceImpl implements NoteService {
     @Override
     public Note add(Note newNote) {
         if (noteRepository.list().stream().anyMatch(note -> note.equals(newNote))) {
+            logger.warning("We have a duplicate!");
             return new Note("", "");
         } else {
             Note note = noteRepository.add(newNote);
+            logger.info("The note was added!");
             return note;
         }
     }
 
     @Override
     public Set<Note> list() {
-        return noteRepository.list();
+        Set<Note> noteSet = noteRepository.list();
+        if (!noteSet.isEmpty()) {
+            noteSet.forEach(note -> logger.info(note.toString()));
+        } else {
+            logger.info("List is empty!");
+        }
+        return noteSet;
     }
 
     @Override
     public Note list(String noteTitle) {
-        return noteRepository.list(noteTitle);
+        Note note = noteRepository.list(noteTitle);
+        if (!note.getNoteTitle().equals("")) {
+            logger.info(String.format("Fond the note: %s", note));
+        } else {
+            logger.info(String.format("Couldn't find the note with the title: %s!", noteTitle));
+        }
+        return note;
     }
 
 }
